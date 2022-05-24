b69604557718edf80dce4f7208fb4ea459c2fe5b
Kapil Kumar <kapilnagarwal@gmail.com>
2022-05-24 14:07:32 +0530

    Create single_element_in_sorted_array.cpp
    Time Complexity: O(log(N))
    
    Space Complexity: O(1)

---
+++ BinarySearch/single_element_in_sorted_array.cpp
@@ -0,0 +1,22 @@
+/* 
+* Question link:  https://leetcode.com/problems/single-element-in-a-sorted-array/submissions/ 
+*/
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size()-2;
+        
+        while(low <= high){
+            int mid = (low + high) >> 1;
+            if (nums[mid] == nums[mid^1]){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return nums[low];
+    }
+};
