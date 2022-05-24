aae6b22776f313a0cefc5a7e93ef38c4dec2b282
Kapil Kumar <kapilnagarwal@gmail.com>
2022-05-24 22:12:40 +0530

    Update Combination_sum_2.cpp

--- Recursion and backtracking/Combination_sum_2.cpp
+++ Recursion and backtracking/Combination_sum_2.cpp
@@ -1,6 +1,12 @@
 /* 
 Question Link : https://leetcode.com/problems/combination-sum-ii/
 
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note: The solution set must not contain duplicate combinations.
+
 */
 
 class Solution {
