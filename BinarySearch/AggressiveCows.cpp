081d6bcbad0d355051d1fc05483b48b0f37ddff8
Kapil Kumar <kapilnagarwal@gmail.com>
2022-05-26 13:26:10 +0530

    Create AggressiveCows.cpp

---
+++ BinarySearch/AggressiveCows.cpp
@@ -0,0 +1,48 @@
+/* 
+ Question Link: https://www.spoj.com/problems/AGGRCOW/
+ 
+ Aggressive Cows : Detailed Solution
+Problem Statement: There is a new barn with N stalls and C cows. The stalls are located on a straight line at positions x1,….,xN (0 <= xi <= 1,000,000,000). We want to assign the cows to the stalls, such that the minimum distance between any two of them is as large as possible. What is the largest minimum distance?
+
+Examples:
+
+Input: No of stalls = 5 
+       Array: {1,2,8,4,9}
+       And number of cows: 3
+
+Output: One integer, the largest minimum distance 3
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+bool isCompatible(vector < int > inp, int dist, int cows) {
+  int n = inp.size();
+  int k = inp[0];
+  cows--;
+  for (int i = 1; i < n; i++) {
+    if (inp[i] - k >= dist) {
+      cows--;
+      if (!cows) {
+        return true;
+      }
+      k = inp[i];
+    }
+  }
+  return false;
+}
+int main() {
+  int n = 5, m = 3;
+  vector<int> inp {1,2,8,4,9};
+  sort(inp.begin(), inp.end());
+  int maxD = inp[n - 1] - inp[0];
+  int ans = INT_MIN;
+  for (int d = 1; d <= maxD; d++) {
+    bool possible = isCompatible(inp, d, m);
+    if (possible) {
+      ans = max(ans, d);
+    }
+  }
+  cout << "The largest minimum distance is " << ans << '\n';
+}
